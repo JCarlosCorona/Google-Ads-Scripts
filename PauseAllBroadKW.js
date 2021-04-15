3f9cd43b1b543e679bc0ef87835ef268c9f1265b
José Carlos Corona <52300353+JCarlosCorona@users.noreply.github.com>
2021-04-15 17:40:35 -0500

    Update PauseAllBroadKW.js

--- PauseAllBroadKW.js
+++ PauseAllBroadKW.js
@@ -9,6 +9,7 @@ var TO_NOTIFY = "your_email@epa.digital";
 function main() {
     var kwIter = AdsApp.keywords()
         .withCondition("KeywordMatchType = BROAD") // pause KW = broad
+        .withCondition("Text DOES_NOT_CONTAIN '+'")
         .forDateRange("ALL_TIME") // could use a specific date range like "20130101","20131231"
         .withCondition("Status = ENABLED")
         .withCondition("CampaignStatus = ENABLED")
